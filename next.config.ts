52da721a118a6669b464a15bf5a49e1ab677889d
Romarichirsein <merveillesitcheu@gmail.com>
2026-05-16 14:27:38 +0100

    chore: add next.config.ts redirects for legacy WordPress URLs

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,40 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:locale/education-et-recherche',
+        destination: '/:locale/institutions?tab=appui',
+        permanent: true,
+      },
+      {
+        source: '/:locale/acteur-publics',
+        destination: '/:locale/institutions?tab=bureaux',
+        permanent: true,
+      },
+      {
+        source: '/:locale/entreprises',
+        destination: '/:locale/institutions?tab=enseignement',
+        permanent: true,
+      },
+      {
+        source: '/:locale/acteur-dappui-au-developpement',
+        destination: '/:locale/institutions?tab=entreprises',
+        permanent: true,
+      },
+      {
+        source: '/:locale/institution-transfrontaliere',
+        destination: '/:locale/institutions?tab=transfrontaliere',
+        permanent: true,
+      },
+      {
+        source: '/:locale/ongs-et-oscs-2',
+        destination: '/:locale/institutions?tab=ongs',
+        permanent: true,
+      }
+    ];
+  }
 };
 
 export default withNextIntl(nextConfig);
